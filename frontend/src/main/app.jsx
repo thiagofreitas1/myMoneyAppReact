5aed441f8aac9b7e79d4b769c32a923f22f77f1e
Thiago Freitas <thiagofreitas.flp@gmail.com>
2020-02-05 20:15:42 -0300

    Continuação do menu

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 import Header from '../common/template/header'
 import SideBar from '../common/template/sidebar'
+
 export default props => (
     <div className='wrapper'>
         <Header />
