f8731b2e0d9c7a6c232762c3dfe9a43fc3313ed0
Thiago Freitas <thiagofreitas.flp@gmail.com>
2020-02-06 10:15:19 -0300

    Inclusão de rotas e template p/ o dashboard

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2020
+            <a href='http://tftecnologia.com.br' target='_blank'> Tf Tecnologia</a>
+        </strong>
+    </footer>
+)
