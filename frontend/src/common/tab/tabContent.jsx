1c3ca0e49330a798b3cebc8ce88775280e8513df
Thiago Freitas <thiagofreitas.flp@gmail.com>
2020-02-13 11:03:12 -0300

    Exibindo os dados da lista

--- frontend/src/common/tab/tabContent.jsx
+++ frontend/src/common/tab/tabContent.jsx
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import If from '../operador/if'
+
 class TabContent extends Component {
     render() {
         const selected = this.props.tab.selected === this.props.id
-        const visible = this.props.tab.visible[this.props.target]
+        const visible = this.props.tab.visible[this.props.id]
         return (
             <If test={visible}>
                 <div id={this.props.id}
