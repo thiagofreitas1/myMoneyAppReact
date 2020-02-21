491651e45fa7795247a017952f5ac50e96b92d37
Thiago Freitas <thiagofreitas.flp@gmail.com>
2020-02-20 21:26:40 -0300

    Novas dependencias do react p/ o frontend

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mymoney', { useMongoClient: true })
+module.exports = mongoose.connect('mongodb://localhost/mymoney', 
+{ useNewUrlParser: true })
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatorio."
 mongoose.Error.messages.Number.min = 
